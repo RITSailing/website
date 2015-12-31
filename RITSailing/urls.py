964e4a5515a4cb1b3b2b0a41db825b9fbe6e7966
Gareth Daunton <gxd5116@rit.edu>
2015-12-31 17:50:19 -0500

    Admin site styling

--- RITSailing/urls.py
+++ RITSailing/urls.py
@@ -36,3 +36,6 @@ urlpatterns = [
     url(r'^inplaceeditform/', include('inplaceeditform.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = 'RIT Sailing Admin'
+admin.site.site_title = 'Sailing Admin'
