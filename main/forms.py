e47f4c8ae2be4eff099057889d601295f4a64501
Gareth Daunton <gxd5116@rit.edu>
2015-12-31 13:28:33 -0500

    Added in user editing.

--- main/forms.py
+++ main/forms.py
@@ -2,6 +2,7 @@ from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 from django.core.mail import send_mail, BadHeaderError
 from django.contrib.auth.models import User
+from phonenumber_field.formfields import PhoneNumberField
 from django import forms
 from models import Request
 import models
@@ -36,3 +37,26 @@ class RegisterForm(forms.Form):
         last_name = self.cleaned_data['last_name']
         year_level = self.cleaned_data['year_level']
         return Request.objects.create_request(email, first_name, last_name, year_level)
+
+class ProfileForm(forms.Form):
+    eboard_pos = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Eboard Position', 'required':'', 'maxlength':"50", "aria-describedby":'addon'}))
+    phone_number = PhoneNumberField(required=False, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Phone Number', "aria-describedby":'addon'}))
+    sailing_level = forms.ChoiceField(choices=models.SAILING_LEVELS, widget=forms.Select(attrs={'class':'form-control'}))
+    year_level = forms.ChoiceField(choices=models.YEAR_LEVELS, widget=forms.Select(attrs={'class':'form-control'}))
+
+    def __init__(self, *args, **kwargs):
+        self.member = kwargs.pop('member', None)
+        self.is_staff = kwargs.pop('is_staff', False)
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        self.fields["eboard_pos"].initial = self.member.eboard_pos
+        self.fields["phone_number"].initial = self.member.phone_number
+        self.fields["sailing_level"].initial = self.member.sailing_level
+        self.fields["year_level"].initial = self.member.year_level
+
+    def save(self, *args, **kwargs):
+        self.member.eboard_pos = self.cleaned_data['eboard_pos']
+        self.member.phone_number = self.cleaned_data['phone_number']
+        if self.is_staff:
+            self.member.sailing_level = self.cleaned_data['sailing_level']
+        self.member.year_level = self.cleaned_data['year_level']
+        return self.member.save()
