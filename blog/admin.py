2de638ed691913ea0e30388c78059cf8152191af
Gareth Daunton <gxd5116@rit.edu>
2016-01-16 18:11:24 -0500

    small fix

--- blog/admin.py
+++ blog/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django import forms
 from ckeditor.widgets import CKEditorWidget
 from models import Post
+from main.models import TeamMember
 
 class PostAdminForm(forms.ModelForm):
     content = forms.CharField(widget=CKEditorWidget())
+    def __init__(self, *args, **kwargs):
+        super(PostAdminForm, self).__init__(*args, **kwargs)
+        # access object through self.instance...
+        self.fields['author'].queryset = TeamMember.objects.filter(user__is_staff=True)
     class Meta:
         model = Post
         fields = '__all__'
