bcc5ad70b1eaca4462c9db6c3c50ad213b466b75
Gareth Daunton <gxd5116@rit.edu>
2016-01-22 12:19:00 -0500

    Blog post page and started event page.

--- events/urls.py
+++ events/urls.py
@@ -3,6 +3,7 @@ from events import views
 
 urlpatterns = [
     url(r'^events/$', views.events, {"template":"main/events.html"}),
+    url(r'^events/(?P<pk>\w+)/$', views.event, name="event"),
     url(r'^events/(?P<pk>\w+)/rsvp/$', views.rsvp, name="rsvp_event"),
-    url(r'^events/(?P<pk>\w+)/rsvp/remove$', views.un_rsvp, name="rm_rsvp_event"),
+    url(r'^events/(?P<pk>\w+)/rsvp/remove/$', views.un_rsvp, name="rm_rsvp_event"),
 ]
